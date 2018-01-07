bbff71fb47509c17fa13af04a5bd51dfa9afb18c
konstantin0s <constantintofan85@gmail.com>
2018-01-07 12:06:22 +0100

    Association between artist and songs plus validation

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ApplicationRecord
+  has_many :songs, dependent: :destroy
+has_many :photos, dependent: :destroy
+
+validates :name, presence: true
+validates :name, uniqueness: true
+#validates :image_url, length: { maximum: 255 }, allow_blank: true
+
+    def self.order_by_name
+     order(:name)
+   end
+
 end
