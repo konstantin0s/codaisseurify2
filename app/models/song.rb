bf4624bb4fce2a8fccac116227046e833cc16876
konstantin0s <constantintofan85@gmail.com>
2018-01-07 12:07:54 +0100

    Add Assoc between songs and artists and validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,12 @@
 class Song < ApplicationRecord
+
+  belongs_to :artist
+
+ validates :name, presence: true
+ validates :artist, presence: true
+
+
+
+ scope :order_by_name, -> { order(:name) }
+
 end
