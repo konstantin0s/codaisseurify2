d17d8796134634f0e301f65ce618a69294680529
konstantin0s <constantintofan85@gmail.com>
2018-01-07 16:23:06 +0100

    Test models

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,43 @@
+require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+
+  context 'validation tests' do
+    it 'ensures name presence' do
+      artist = Artist.new(name: 'Pavaroti').save
+        end
+
+    it 'ensures image_url presence' do
+        end
+
+    it 'should save successfully' do
+        end
+  end
+
+
+
+  describe "association with songs" do
+      artist = Artist.create!( name: Faker::Lovecraft.tome )
+
+      song1 = Song.create!(name: Faker::Lovecraft.tome, artist: artist )
+      song2 = Song.create!(name: Faker::Lovecraft.tome, artist: artist )
+
+      it "has many songs" do
+        expect(artist.songs).to include(song1)
+        expect(artist.songs).to include(song2)
+      end
+
+      it "deletes associated songs" do
+      expect { artist.destroy }.to change(artist.songs, :count).by(-2)
+      end
+  end
+
+
+
+  context 'scope tests' do
+
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
+
+
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  context 'validation tests' do
+    it 'ensures name presence' do
+      song = Song.new(name: 'Masquerada').save
+        end
+
+
+
+    it 'should save successfully' do
+      song = Song.new(name: 'Masquerada').save
+      expect(song).to eq(song)
+        end
+  end
+
+
+
+  describe "song association with artist" do
+
+      artist = Artist.create!(name: "Super Karma")
+
+    it "belongs to artist" do
+      song = artist.songs.new
+
+      expect(song.artist).to eq(artist)
+    end
+
+  end
+
+
 end
