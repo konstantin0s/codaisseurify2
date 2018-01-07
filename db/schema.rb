0ddec3ff843c26da392a64ad416322c379fc939d
konstantin0s <constantintofan85@gmail.com>
2018-01-07 12:14:43 +0100

    add photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107110109) do
+ActiveRecord::Schema.define(version: 20180107111009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180107110109) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.bigint "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_photos_on_artist_id"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -29,5 +37,6 @@ ActiveRecord::Schema.define(version: 20180107110109) do
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "photos", "artists"
   add_foreign_key "songs", "artists"
 end
